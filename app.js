8f1f5565f7b19f2274234af60a4163636c0c599a
SantyCr <77855047+SantyCr@users.noreply.github.com>
2022-01-28 23:02:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -33,24 +33,8 @@ const sizePage = 10;
 
 async function listarCurso (){
     const curso = await Curso
-    //.find({autor : 'Grover'})
-    //.find({precio: {$gte:10, $lte : 30 }})
-    .find({precio: {$in: [10, 25, 30] }})
-
-    //OPERADORES LOGICOS
-    //.or([{autor: 'Grover'}, {publicado: true}])
-    .and([{autor: 'Grover'}, {publicado: true}])
-
-    //EXPRECCIONES REGULARES
-
-    /* //EMPIECE CON LA PALABRA Gro
-    .find({ autor: /^Gro/})
-    
-    //TERMINA CON LA PALABRA ver
-    .find({ autor: /ver$/ }) */
-
-    //Cuando un campo tiene un contenido  especifico
     .find( {autor:  /.*ro.*/} )
+    .and([{autor: 'Grover'}, {publicado: true}])
     .skip( (numeroPage - 1)* sizePage)
     .limit(10)
     .sort({autor : -1})
@@ -59,31 +43,6 @@ async function listarCurso (){
 listarCurso();
 
 async function actualizarCurso (id){
-    /* const curso = await Curso.findById(id)
-    if(!curso) {console.log('El curso no existe')
-    return;}
-
-    // FORMA ONE
-    curso.publicado = false;
-    curso.autor = 'Grover'; 
-
-    // FORMA TWO
-    curso.set({
-        publicado: false,
-        autor: "Grove vazquez"
-    }) 
-
-
-    const resultado = await curso.save(); */
-    
-    //FORMA TREE
-    /* const resultado = await Curso.update({ _id: id},{
-        $set: {
-            autor: "Grover2",
-            publicado: true
-        }
-    }) */
-
     const resultado = await Curso.findByIdAndUpdate(id,{
         $set: {
             autor: "Luis Perez",
@@ -96,7 +55,6 @@ async function actualizarCurso (id){
 actualizarCurso("61f42532fdc55c9312fff7ae");
 
 async function eliminarDocumento (){
-    const resutado = await Curso.deleteOne({ _id: id})
-    //const resutado = await Curso.findByIdAndDelete(id)
+    const resutado = await Curso.findByIdAndDelete(id)
     console.log(resutado);
 }
